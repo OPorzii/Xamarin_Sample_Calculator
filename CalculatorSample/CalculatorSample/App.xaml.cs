21296e8e6797815ba346cdd4f96751f6be33584d
OPorzii <59070103@it.kmitl.ac.th>
2019-07-22 15:19:08 +0700

    First commit

---
+++ CalculatorSample/CalculatorSample/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using CalculatorSample.ViewModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorSample
+{
+    public partial class App : Application
+    {
+
+        private static ViewModelLocator _locator;
+        public static ViewModelLocator Locator
+        {
+            get
+            {
+                return _locator = _locator ?? new ViewModelLocator();
+            }
+        }
+
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ CalculatorSample/CalculatorSample/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorSample.ViewModels
+{
+    using Unity;
+    using ViewModels;
+
+    public class ViewModelLocator
+    {
+        private readonly IUnityContainer _container;
+
+        public ViewModelLocator()
+        {
+            _container = new UnityContainer();
+
+            // ViewModels
+            _container.RegisterType<CalculatorViewModel>();
+        }
+
+        public CalculatorViewModel CalculatorViewModel
+        {
+            get { return _container.Resolve<CalculatorViewModel>(); }
+        }
+    }
+}
